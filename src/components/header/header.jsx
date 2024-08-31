74ad863af90511acb56be238a2db8aae0dcce78b
Brandon Gonzalez <brajgon@gmail.com>
2024-08-30 22:43:34 -0600

    working on styling header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,20 @@
+import "./header.css";
+
 const Header = () => {
-    return (
-        <div className="header">
-            <p>Brandon Gonzalez | Portfolio</p>
-        </div>
-    )
-}
+  return (
+    <header className="header">
+      <p>Brandon Gonzalez | Portfolio</p>
+      <nav className="nav">
+        <a href="http://github.com/brajgon1">GitHub</a>
+
+        <a href="http://www.linkedin.com/in/brandon-gonzalez-022389133/">
+          LinkedIn
+        </a>
+
+        <a href="mailto:gonzb820@gmail.com">Email</a>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
